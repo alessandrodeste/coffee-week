c65b9ee698533398ac8b2f16639b647dd05d2c99
Alessandro d'Este <alessandrodeste@gmail.com>
2018-09-13 18:54:38 +0100

    card design

--- src/components/PairCard/PairCard.js
+++ src/components/PairCard/PairCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {stringToColour} from '../../helpers/utility';
+import CoffeeBeans from '../../assets/coffee_beans.svg';
 import './PairCard.css';
 
 const PairCard = ({giver, receiver}) => {
@@ -8,10 +9,36 @@ const PairCard = ({giver, receiver}) => {
   };
   return (
     <div className="pairCard">
-      <div className="giver">{getFullName(giver.name)}</div>
-      <div className="receiver">{getFullName(receiver.name)}</div>
+      <div className="pairCard-giver">
+        <div className="pairCard-motto">
+          &ldquo;
+          {giver.motto}
+          &rdquo;
+        </div>
+        <div className="pairCard-info">
+          {giver.email} / {giver.phone}
+        </div>
+        <div className="pairCard-name">{getFullName(giver.name)}</div>
+      </div>
+      <img
+        src={CoffeeBeans}
+        className="pairCard-image"
+        alt="coffee bean"
+        width="48"
+      />
+      <div className="pairCard-receiver">
+        <div className="pairCard-name">{getFullName(receiver.name)}</div>
+        <div className="pairCard-info">
+          {receiver.email} / {receiver.phone}
+        </div>
+        <div className="pairCard-motto">
+          &ldquo;
+          {receiver.motto}
+          &rdquo;
+        </div>
+      </div>
       <div
-        className="group"
+        className="pairCard-group"
         style={{
           backgroundColor: stringToColour(giver.location + giver.department),
         }}
